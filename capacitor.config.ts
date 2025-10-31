432979e2f6d88a49f5a684e44e3a65d21d884574
omotayojoe2001 <joshuatimilehin1010@gmail.com>
2025-10-31 18:52:46 +0100

    describe what you changed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tradehub.centralexchange',
+  appId: 'com.centralexchange.global',
   appName: 'Central Exchange',
   webDir: 'dist',
   server: {
